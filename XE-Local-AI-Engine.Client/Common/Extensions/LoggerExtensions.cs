fefca3e9e876477080a031a673749e139182f6ee
w0rldx <michaelgmx1@googlemail.com>
2026-04-14 17:26:29 +0200

    move logger and reformat

---
+++ XE-Local-AI-Engine.Client/Common/Extensions/LoggerExtensions.cs
@@ -0,0 +1,27 @@
+namespace XE_Local_AI_Engine.Client.Common.Extensions;
+
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+using ILogger = Serilog.ILogger;
+
+public static class LoggerExtensions
+{
+    public static ILogger CreateStartupLogger(this IHostEnvironment environment)
+    {
+        ArgumentNullException.ThrowIfNull(environment);
+
+        const string ConsoleOutputTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {Message:lj}{NewLine}{Exception}";
+
+        var loggerConfiguration = new LoggerConfiguration()
+                                  .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                                  .Enrich.FromLogContext()
+                                  .WriteTo.Console(theme: ConsoleTheme.None, outputTemplate: ConsoleOutputTemplate);
+
+#pragma warning disable CA2000 // Ownership is transferred to Log.Logger and released via Log.CloseAndFlushAsync in finally.
+        return environment.IsEnvironment("Testing")
+            ? loggerConfiguration.CreateLogger()
+            : loggerConfiguration.CreateBootstrapLogger();
+#pragma warning restore CA2000
+    }
+}
